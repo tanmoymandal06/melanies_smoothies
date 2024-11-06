4828d2887e36ddd5ff5796820b2ec5030d865d4a
tanmoymandal06 <149082884+tanmoymandal06@users.noreply.github.com>
2024-11-07 00:22:30 +0530

    Update streamlit_app.py
    indentation undo

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,18 +44,18 @@ if ingredients_list:
 
         # st.write(ingredients_string)
     
-        my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
     
     # st.write(my_insert_stmt)
     # st.stop()
     
-        time_to_insert = st.button('Submit Order')
+    time_to_insert = st.button('Submit Order')
     
-        if time_to_insert:
-            session.sql(my_insert_stmt).collect()
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
         
-            st.success('Your Smoothie is ordered,'+name_on_order+'!', icon="✅")
+        st.success('Your Smoothie is ordered,'+name_on_order+'!', icon="✅")
 
 # New section to display fruityvice nutrition information 
 
